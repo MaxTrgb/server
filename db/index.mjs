0dbdfaf8ed796cdc2c34305feeaef7809e53640d
Max <126002400+MaxTrgb@users.noreply.github.com>
2024-09-04 21:11:13 +0300

    fixed braces

--- db/index.mjs
+++ db/index.mjs
@@ -6,6 +6,6 @@ dotenv.config();
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 
-mongoose.connect('mongodb+srv://${dbUsername}:${dbPassword}@cluster0.gucfv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(`mongodb+srv://${dbUsername}:${dbPassword}@cluster0.gucfv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
 
 export default mongoose.connection;
